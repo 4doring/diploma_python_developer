5c2d8ee1bbaf0163d8c3c0a897a6344ea86fdf51
unknown <bb4@mail.ru>
2025-10-06 15:56:36 +0300

    added models.py for catalog, contacts, orders

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Модель контактной информации пользователя.
+    """
+
+    objects = models.manager.Manager()
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        related_name="contacts",
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    city = models.CharField(max_length=50, verbose_name="Город", blank=True)
+    street = models.CharField(max_length=100, verbose_name="Улица", blank=True)
+    house = models.CharField(max_length=15, verbose_name="Дом", blank=True)
+    structure = models.CharField(max_length=15, verbose_name="Корпус", blank=True)
+    building = models.CharField(max_length=15, verbose_name="Строение", blank=True)
+    apartment = models.CharField(max_length=15, verbose_name="Квартира", blank=True)
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+
+    class Meta:
+        verbose_name = "Контакт пользователя"
+        verbose_name_plural = "Список контактов пользователя"
+
+    def __str__(self):
+        return f"{self.city} {self.street} {self.house}"
